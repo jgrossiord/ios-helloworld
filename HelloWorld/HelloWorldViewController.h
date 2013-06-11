83020edebe2bd1bdb6a7cc4fde7ee73638b059c0
Julien Grossiord <jgrossiord@mediastay.com>
2013-06-11 17:57:36 +0200

    Initial Commit

---
+++ HelloWorld/HelloWorldViewController.h
@@ -0,0 +1,13 @@
+//
+//  HelloWorldViewController.h
+//  HelloWorld
+//
+//  Created by Julien Grossiord on 11/06/13.
+//  Copyright (c) 2013 Julien Grossiord. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController
+
+@end
---
+++ HelloWorld/HelloWorldViewController.m
@@ -0,0 +1,29 @@
+//
+//  HelloWorldViewController.m
+//  HelloWorld
+//
+//  Created by Julien Grossiord on 11/06/13.
+//  Copyright (c) 2013 Julien Grossiord. All rights reserved.
+//
+
+#import "HelloWorldViewController.h"
+
+@interface HelloWorldViewController ()
+
+@end
+
+@implementation HelloWorldViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
